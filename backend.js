f3573cf58efcf9db5e41a0b2979cbff5150b4216
lawfuhin <lawfuhin@gmail.com>
2023-03-25 20:32:48 +0800

    first commit

--- backend.js
+++ backend.js
@@ -6,7 +6,7 @@ var cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 80;
+const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
